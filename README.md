2d691fdeaaa256029fb67c21886293ea415b40ed
jasongoodwin <jay.michael.goodwin@gmail.com>
2014-03-01 14:45:43 -0500

    add verbatim to fix problem with conf. update conf and readme

--- README.md
+++ README.md
@@ -2,4 +2,13 @@ Scala Smoke template
 ====================
 
 A g8 project for Smoke applications.
-*not tested/stable yet*
+
+Contents
+--------
+This is a skinny sample
+
+Gets you started with:
+- Basic example app
+- ScalaTest run against the example app
+- Basic config is in place (Typesafe Config)
+- Static assets
--- src/main/g8/src/main/scala/App.scala
+++ src/main/g8/src/main/scala/App.scala
@@ -1,19 +1,25 @@
-import smoke._
 import com.typesafe.config.ConfigFactory
+import smoke._
 
-object BasicExampleApp extends App {
-    val smoke = new BasicExampleSmoke
+object BasicExampleApp extends SmokeApp with StaticAssets {
+
+  override val smokeConfig = ConfigFactory.load().getConfig("smoke")
+  override val executionContext = scala.concurrent.ExecutionContext.global
+
+  override val publicFolder: String = smokeConfig.getString("static-assets.public-dir")
+  override val cacheAssets: Boolean = smokeConfig.getBoolean("static-assets.cache-assets")
+  override val cacheAssetsPreload: Boolean = smokeConfig.getBoolean("static-assets.cache-assets-preload")
+
+  onRequest {
+    case GET(Path(path)) ⇒ reply(responseFromAsset(path))
+    case _               ⇒ reply(Response(NotFound))
+  }
+
+  after { response ⇒
+    val headers = response.headers ++ Seq(
+      "Server" -> "MockFeatures",
+      "Connection" -> "Close")
+    Response(response.status, headers, response.body)
+  }
 }
 
-class BasicExampleSmoke extends Smoke {
-    val config = ConfigFactory.load().getConfig("smoke")
-    val executionContext = scala.concurrent.ExecutionContext.global
-
-    onRequest {
-        case GET(Path("/example")) ⇒ reply {
-            Thread.sleep(1000)
-            Response(Ok, body = "It took me a second to build this response.\n")
-        }
-        case _ ⇒ reply(Response(NotFound))
-    }
-}
